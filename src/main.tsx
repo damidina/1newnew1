b568af24c0b1e83bf125c7f247896adc784be8c8
Dami Dina <damidina@users.noreply.github.com>
2026-02-14 14:36:51 -0500

    feat: complete scaffolding

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div className="p-20 text-center">
+        <h1 className="text-2xl font-bold">Welcome to 1newnew1</h1>
+        <p className="text-slate-400 mt-2">Edit src/App.tsx to get started.</p>
+    </div>
+  </React.StrictMode>,
+)
